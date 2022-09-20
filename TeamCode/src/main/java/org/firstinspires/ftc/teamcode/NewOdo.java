1c7faef68cf596a2e12b7eecfe39bc8f2db5cf8e
Benjamin <23besebe@students.lovelandschools.org>
2022-09-19 20:50:32 -0400

    I mostly finished the updateLocalPosition class in the newOdo class, which included adding a lot of variables -Ben

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewOdo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewOdo.java
@@ -1,18 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.openftc.revextensions2.ExpansionHubEx;
 import org.openftc.revextensions2.RevBulkData;
 
 public class NewOdo extends Mechanism{
 
 
+    private final ExpansionHubEx expansionHub;
+    //Bulk data
     public RevBulkData bulkData;
+    //bulkData.getMotorCurrentPosition(i) gets the total ticks rotated since power up
+
     /*
     Bulk read assignments
     0 = Left
     1 = Right
-    2 = Middle
+    2 = Back
     */
 
     //Declares constants that relate to odometry wheels
@@ -37,42 +43,83 @@ public class NewOdo extends Mechanism{
     public double wheel_circumference = effective_diameter * Math.PI;
 
 
-    //Previous distance that left odometry module has rotated
-    public double preLeftPosition = 0;
-    //Previous distance that right odometry module has rotated
-    public double preRightPosition = 0;
-    //Previous distance that back odometry module has rotated
-    public double preBackPosition = 0;
-    //Change in left odometry module spin distance since last update
+    //Distance the odometry wheel was at last cycle
+    public double previousLeftPosition = 0;
+    public double previousRightPosition = 0;
+    public double previousBackPosition = 0;
+
+    //Change in odometry module spin distance in MM since last cycle
     public double deltaLeftMM = 0;
-    //Change in right odometry module spin distance since last update
     public double deltaRightMM = 0;
-    //Change in back odometry module spin distance since last update
     public double deltaBackMM = 0;
 
-    public double deltaLeftTicks = 0;
-    public double deltaRightTicks = 0;
-    public double deltaBackTicks = 0;
+    //The amount of ticks that are left over from a bulkdata pull before an odometry reset
+    public double junkLeftTicks = 0;
+    public double junkRightTicks = 0;
+    public double junkBackTicks = 0;
 
+    //The amount of ticks rotated after an odometry reset
+    public double postResetLeftTicks = 0;
+    public double postResetRightTicks = 0;
+    public double postResetBackTicks = 0;
+
+    //Change in rotation in radians since last cycle
+    private double deltaLocalRotation = 0;
     //Distance between robot's center and center of arc rotation
     private double rT = 0;
+    //Change in straight line distance since last cycle
+    private double deltaLocalDistance = 0;
+    //Change in local coordinates that the robot moved since last cycle
+    private double  deltaLocalX = 0;
+    private double deltaLocalY = 0;
+    //Radius of the strafe to arc center
+    private double rS = 0;
+    //Change in local coordinates that the robot strafed since last cycle
+    private double deltaXStrafe = 0;
+    private double deltaYStrafe = 0;
 
+
+    public NewOdo(HardwareMap hardwareMap) {
+        expansionHub = hardwareMap.get(ExpansionHubEx.class, "Expansion Hub 1");
+        //reset();
+    }
 
     public void updateLocalPosition() {
-        rT = (deltaLeftMM+deltaRightMM)/(deltaLeftMM-deltaRightMM);
+        bulkData = expansionHub.getBulkInputData();
 
+        //Calculates the amount of ticks spun since reset by subtracting the junk from before rest from total rotated
+        postResetLeftTicks = bulkData.getMotorCurrentPosition(0) - junkLeftTicks;
+        postResetRightTicks = bulkData.getMotorCurrentPosition(1) - junkRightTicks;
+        postResetBackTicks = bulkData.getMotorCurrentPosition(2) - junkBackTicks;
+
+
+        //Converts change in ticks from last cycle into change in MM from last cycle for each wheel, and 1 & 2 are inversed
         for (int i = 0; i == 2; i++) {
-            if (i == 1) {
-                //Converts change in ticks from last step into change in MM from last step for the left wheel
-                deltaLeftMM = -wheel_circumference * (bulkData.getMotorCurrentPosition(i) - deltaLeftTicks + (preLeftPosition) / encoder_ticks);
-            } else if (i == 2) {
-                //Converts change in ticks from last step into change in MM from last step for the right wheel
-                deltaRightMM = -wheel_circumference * (bulkData.getMotorCurrentPosition(i) - deltaRightTicks + (preRightPosition) / encoder_ticks);
+            if (i == 0) {
+                deltaLeftMM = wheel_circumference * ((postResetLeftTicks - previousLeftPosition) / encoder_ticks);
+            } else if (i == 1) {
+                deltaRightMM = -wheel_circumference * ((postResetRightTicks + previousRightPosition) / encoder_ticks);
             } else {
-                //Converts change in ticks from last step into change in MM from last step for the back wheel
-                deltaBackMM = -wheel_circumference * (bulkData.getMotorCurrentPosition(i) - deltaBackTicks + (preBackPosition) / encoder_ticks);
+                deltaBackMM = -wheel_circumference * ((postResetBackTicks + previousBackPosition) / encoder_ticks);
             }
         }
+        //Displays telemetry for the change in MM from last cycle for each wheel
+        addPublicTelemetry("",""+deltaLeftMM);
+        addPublicTelemetry("",""+deltaRightMM);
+        addPublicTelemetry("",""+deltaBackMM);
+
+        deltaLocalRotation = (deltaLeftMM-deltaRightMM) / (left_offset + right_offset);
+        rT = (deltaLeftMM + deltaRightMM) / (deltaLeftMM - deltaRightMM);
+        deltaLocalDistance = 2 * rT * Math.sin(deltaLocalRotation / 2);
+        deltaLocalX = rT * (1 - Math.cos(deltaLocalRotation));
+        deltaLocalY = rT * Math.sin(deltaLocalRotation);
+        rS = (deltaBackMM / deltaLocalRotation) - back_offset;
+        deltaXStrafe = rS * Math.sin(deltaLocalRotation);
+        deltaYStrafe = rS * (Math.cos(deltaLocalRotation) - 1);
+    }
+
+    public void updateGlobalPosition() {
+
     }
 
 
