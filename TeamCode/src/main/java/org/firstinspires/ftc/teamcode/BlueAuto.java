97bf23245e7a7f865f21f09cf19a977e04f00a76
Benjamin Beseler <23besebe@students.lovelandschools.org>
2022-10-02 19:56:28 -0400

    Minor changes to IntakeOp and DriveOpMode, wrote base for BlueAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuto.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+@Autonomous(name="BlueAuto", group="autonomous")
+public class BlueAuto extends LinearOpMode {
+    public Robot robot;
+    public Drivetrain drivetrain;
+    public String[] motorNames = {"frontRight","frontLeft","backLeft","backRight"};
+    public int[] motorNumbers = {0, 1, 2, 3}; //creates motor numbers array
+    public Output output;
+
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(this);
+        drivetrain = new Drivetrain(robot, motorNumbers);
+        output = new Output(hardwareMap.get(Servo.class, "outputServo"));
+        Deadline stop = new Deadline(500, TimeUnit.MILLISECONDS);
+        waitForStart();
+        stop.reset();
+        drivetrain.motors.get(0).setPower(0.5);
+        drivetrain.motors.get(1).setPower(0.5);
+        drivetrain.motors.get(2).setPower(0.5);
+        drivetrain.motors.get(3).setPower(0.5);
+        if(stop.hasExpired()){
+            drivetrain.motors.get(0).setPower(0);
+            drivetrain.motors.get(1).setPower(0);
+            drivetrain.motors.get(2).setPower(0);
+            drivetrain.motors.get(3).setPower(0);
+        }
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeOp.java
@@ -2,12 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name = "IntakeOp")
 public class IntakeOp extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-
 
         Intake intake = new Intake(hardwareMap.get(DcMotorEx.class, "IntakeMotor"));
 
