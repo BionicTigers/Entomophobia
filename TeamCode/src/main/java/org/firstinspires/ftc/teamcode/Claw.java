9ba57395c6fdb59337d70bec780a7aac7c49edad
Benjamin Beseler <23besebe@students.lovelandschools.org>
2022-11-04 11:10:12 -0400

    New set position lift code. uses D-Pad and right stick on GP2. -Benjamin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -9,17 +9,22 @@ public class Claw extends Mechanism{
 
     public Claw (Servo l, Servo r) {
         super();
+        //Servos.get(0), bigger # = Counter-clockwise
         servol = l;
         servos.add(servol);
+        //Servos.get(1), bigger # = Counter-clockwise
         servor = r;
         servos.add(servor);
     }
+
     @Override
     public void update(Gamepad gp1, Gamepad gp2) {
         if (gp1.right_trigger > 0.3) {
+            //Closes the claw
             servos.get(0).setPosition(0.5);
             servos.get(1).setPosition(0.5);
         } else if (gp1.left_trigger > 0.3) {
+            //Opens the claw
             servos.get(0).setPosition(0.75);
             servos.get(1).setPosition(0.25);
         }
