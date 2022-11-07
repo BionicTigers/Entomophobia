88eff5e787d01b9cdc600390b710703c67884512
Benjamin Beseler <23besebe@students.lovelandschools.org>
2022-11-07 10:39:29 -0500

    Some set lift position updates to let it go to the right position -Benjamin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -13,18 +13,22 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  */
 public class Lift extends Mechanism {
     public Telemetry telemetry;
-
+    public int height = 0;
+    public int trim = 0;
     /**
      * Adds motors to the left and right variables
      * @param l imported left motor
      */
-    public Lift (DcMotorEx l) {
+    public Lift (DcMotorEx l, Telemetry T) {
         super();
         motors.add(l);
         motors.get(0).setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         motors.get(0).setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         motors.get(0).setTargetPosition(0);
         motors.get(0).setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+        telemetry = T;
+        height = 0;
     }
 
     /**
@@ -33,20 +37,55 @@ public class Lift extends Mechanism {
      * @param gp2 the lift gamepad
      */
     public void update(Gamepad gp1, Gamepad gp2) {
-        if (gp2.right_stick_y >= 0.25) {
-            motors.get(0).setTargetPosition(50);
+//        if (gp2.right_stick_y >= 0.25) {
+//            height = height + 10;
+//        }
+//        else if (gp2.right_stick_y <= -0.25) {
+//            height = height - 10;
+//        } else {
+//            height = height;
+//        }
+//        if (height > 0) {
+//            height = 0;
+//        }
+
+        if (gp2.dpad_up) {
+            height = -2000;
+        } else if (gp2.dpad_down) {
+            height = 0;
+            if (trim < 0) {
+                trim = 0;
+            }
         }
-        else if (gp2.right_stick_y <= -0.25) {
-            motors.get(0).setTargetPosition(100);
+
+        if (gp2.right_stick_y >= 0.3) {
+            trim = trim + 10;
         }
-        else {
-            motors.get(0).setTargetPosition(0);
+        if (gp2.right_stick_y <= 0.3) {
+            trim = trim - 10;
         }
-        telemetry.addData("Current position: ", motors.get(0).getCurrentPosition());
     }
+
+//    //FOR SHOWCASE NIGHT USE ONLY
+//    public void update(Gamepad gp1, Gamepad gp2) {
+//        if (gp1.right_trigger >= 0.25) {
+//            height = height + 10;
+//        }
+//        else if (gp1.left_trigger >= 0.25) {
+//            height = height - 10;
+//        }
+//        telemetry.addData("Current position: ", motors.get(0).getCurrentPosition());
+//    }
 
     @Override
     public void write() {
+        //Sets the height of the lift to height + trim
+        motors.get(0).setTargetPosition(height + trim);
+        //Makes the lift motor move
         motors.get(0).setVelocity(1000);
+
+        //Provides telemetry for the motor's current position and the trim value
+        telemetry.addData("Current position: ", motors.get(0).getCurrentPosition());
+        telemetry.addData("Trim", trim);
     }
 }
