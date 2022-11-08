ddde88fcc0f535a5a273624a1c45794da0e8246d
Alex <alexkaiser32123@gmail.com>
2022-11-07 20:43:41 -0500

    Added TFOD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypeOp.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp(name = "Prototype")
 public class PrototypeOp extends LinearOpMode {
 
     @Override
