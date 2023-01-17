ac354234932b5edaa33585d59fc06c200191db50
ATK <54485972+ATK422@users.noreply.github.com>
2023-01-16 20:41:14 -0500

    Added ControlHub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ControlHub.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.qualcomm.hardware.lynx.LynxDcMotorController;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.List;
+
+public class ControlHub {
+    LynxDcMotorController hub;
+    int[] bulkDataCache;
+
+    public ControlHub(HardwareMap hardwareMap, LynxDcMotorController hub) {
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        this.hub = hub;
+        bulkDataCache = this.internalRefreshBulkData();
+    }
+
+    private int[] internalRefreshBulkData() {
+        int[] bulkData = new int[4];
+
+        for (int i=0; i<4; i++) {
+            bulkData[i] = hub.getMotorCurrentPosition(i);
+        }
+
+        return bulkData;
+    }
+
+    public void refreshBulkData() {
+        bulkDataCache = this.internalRefreshBulkData();
+    }
+
+    public int getEncoderTicks(int motor) {
+        return bulkDataCache[motor];
+    }
+}
