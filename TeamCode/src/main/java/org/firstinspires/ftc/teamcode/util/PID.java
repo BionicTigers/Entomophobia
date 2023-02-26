198c6998c0d6049d2141c0e80f43fa09dca0ed0b
BenjaminBeseler <23besebe@students.lovelandschools.org>
2023-02-26 17:41:50 -0500

    Updated PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PID.java
@@ -1,37 +1,52 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class PID {
     double Kp;
     double Ti; //In Minutes
     double Td; //In Minutes
 
-    double min;
-    double max;
+    double minPV;
+    double maxPV;
 
-    double sampleTime = 100; //ms
+    double minCV;
+    double maxCV;
+
+    double sampleTime = 20; //ms
 
     ElapsedTime previousCall;
 
-    double E1; //1st Previous Error
-    double E2; //2nd Previous Error
+    Telemetry telem = FtcDashboard.getInstance().getTelemetry();
+
+    double PV1; //1st Previous Error
+    double PV2; //2nd Previous Error
+
+    double E1;
 
     double CV; //Current Value
 
-    public PID(double Kp, double Ti, double Td, double min, double max) {
+    public PID(double Kp, double Ti, double Td, double minPV, double maxPV, double minCV, double maxCV) {
         //Set Terms
         this.Kp = Kp;
         this.Ti = Ti;
         this.Td = Td;
 
         //Set Limits
-        this.min = min;
-        this.max = max;
+        this.minPV = minPV;
+        this.maxPV = maxPV;
+
+        this.minCV = minCV;
+        this.maxCV = maxCV;
 
         //Set Defaults
+        PV1 = 0;
+        PV2 = 0;
+
         E1 = 0;
-        E2 = 0;
 
         CV = 0;
 
@@ -43,25 +58,33 @@ public class PID {
         double dt = previousCall.milliseconds() / 1000;
 
         if (dt > sampleTime / 1000) {
-            //Turn Error into a percentage
-            double E = (SP - PV) / (max - min);
+            //Turn to percentage
+            double scaledPV = (maxPV - minPV);
+            double E = (SP - PV) / (maxPV - minPV);
 
-            double P = E;
+            double P = E - E1;
 
             double I;
             if (Ti == 0) //Cannot divide by 0 so we set it to 0
                 I = 0;
             else
-                I = Kp * (1 / (60 * Ti) * E * dt);
+                I = (E * dt) / (60 * Ti);
 
-            double D = 60 * Td * ((E2 * E1 + E) / dt);
+            double D = 60 * Td * ((scaledPV - 2 * PV1 + PV2) / dt);
+
+            telem.addData("E", E * (maxPV - minPV));
+            telem.addData("P", Kp * P);
+            telem.addData("I", Kp * I);
+            telem.addData("D", Kp * D);
 
             //Clamp CV to the min and the max
-            CV = Math.max(min, Math.min(max, CV + Kp * (P + I + D)));
+            CV = Math.max(minCV, Math.min(maxCV, CV + Kp * (P + I + D)));
 
             //Set Futures
-            E2 = E1;
             E1 = E;
+
+            PV2 = PV1;
+            PV1 = scaledPV;
 
             //Reset Elapsed Time
             previousCall.reset();
